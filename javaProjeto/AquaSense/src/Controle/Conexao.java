2cfc7ae54341ceec4ec43705ecb3b225d3f41fa5
Eliharison <eliharison11@gmail.com>
2023-10-24 19:42:32 -0300

    Adicionando MVC

---
+++ javaProjeto/AquaSense/src/Controle/Conexao.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controle;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author dsm2
+ */
+public class Conexao {
+        
+    final private String driver = "com.mysql.jdbc.Driver";
+   
+    final private String url= "jdbc:mysql://127.0.0.1/NOMEBANCO";
+    
+    final private String usuario="root";
+    final private String senha="";
+    private Connection conexao;// objeto que faz conexao com o banco
+    public Statement statement;// objeto que abre caminho até o banco
+    public ResultSet resultset;// objeto que armazena os comandos sql   
+    
+    public boolean conecta() {  
+        boolean result = true;  
+  
+        try {  
+            Class.forName(driver);  
+            conexao = DriverManager.getConnection(url,usuario,senha);
+            //JOptionPane.showMessageDialog(null,"Conectou com o Banco de Dados");
+            
+        } catch(ClassNotFoundException Driver){
+               JOptionPane.showMessageDialog(null,"Driver nao localizado: "+Driver);
+               result = false;
+        }catch(SQLException Fonte) {
+                JOptionPane.showMessageDialog(null,"Erro na conexão com a fonte de dados: "+Fonte);
+                result = false;
+            }
+        return result;  
+    }
+    
+     public void desconecta (){
+        boolean result = true;
+        try
+        {
+            conexao.close();
+            //JOptionPane.showMessageDialog(null,"Banco fechado");
+        }
+        catch(SQLException fecha)
+        {
+            JOptionPane.showMessageDialog(null,"Não foi possivel fechar o banco de dados"+ fecha);
+            result = false;
+        }
+    }
+     
+     public void executeSQL(String sql){
+//chamada do metodo conecta para abrir a conexão com o db
+        conecta();
+        try{
+    
+        statement = conexao.createStatement();
+
+        statement.execute(sql);
+        //desconecta();
+        }catch(SQLException sqle){
+           JOptionPane.showMessageDialog(null, "Driver não encontrado1" + sqle.getMessage());
+        }
+}
+    
+    public ResultSet RetornarResultset(String sql){
+         ResultSet resultSet = null;
+         conecta();
+         try{
+         statement = conexao.createStatement();
+         resultSet = statement.executeQuery(sql);
+         resultSet.next();
+         }catch (Exception e){
+         JOptionPane.showMessageDialog(null, "Erro ao retornar resultset"+e.getMessage());
+         }
+         return resultSet;
+ }
+}
---
+++ javaProjeto/AquaSense/src/Modelo/Usuario.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+import Controle.Conexao;
+
+/**
+ *
+ * @author dsm2
+ */
+public class Usuario {
+    
+    Conexao C = new Conexao();
+    
+    
+}
