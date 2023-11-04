537c1e81e71f254cc8dc48d4b84db80367a27a7c
RodrigoS <2005rodrigosilva@gmail.com>
2023-11-04 09:36:46 -0300

    Alterando Classe Tanque

--- src/Modelo/Alerta.java
+++ src/Modelo/Alerta.java
@@ -52,5 +52,5 @@ public class Alerta {
         this.dtHora = dtHora;
     }
     
-    
+        
 }
--- src/Modelo/tanque.java
+++ src/Modelo/tanque.java
@@ -14,25 +14,71 @@ import java.util.List;
  * @author dsm2
  */
 public class tanque {
-    private float capacidade;
-    private float nivelVolume;
-    private float temperaturaAgua;
-    private float pHAgua;
-    private Float oxigenacao;
-    private List<Peixe> peixes;
-    
-    Conexao C = new Conexao();
+    private double capacidade;
+    private double nivelVolume;
+    private double temperaturaAgua;
+    private double pHAgua;
+    private double oxigenacao;
+    private double peixes;
 
     public tanque() {
-        this(0.0f,0.0f,0.0f,0.0f,0.0f, new ArrayList<>());
+        this(0.0,0.0,0.0,0.0,0.0,0.0);
     }
 
-    public tanque(float capacidade, float nivelVolume, float temperaturaAgua, float pHAgua, Float oxigenacao, List<Peixe> peixes) {
+    public tanque(double capacidade, double nivelVolume, double temperaturaAgua, double pHAgua, double oxigenacao, double peixes) {
         this.capacidade = capacidade;
         this.nivelVolume = nivelVolume;
         this.temperaturaAgua = temperaturaAgua;
         this.pHAgua = pHAgua;
         this.oxigenacao = oxigenacao;
+        this.peixes = peixes;
+    }
+
+    public double getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(double capacidade) {
+        this.capacidade = capacidade;
+    }
+
+    public double getNivelVolume() {
+        return nivelVolume;
+    }
+
+    public void setNivelVolume(double nivelVolume) {
+        this.nivelVolume = nivelVolume;
+    }
+
+    public double getTemperaturaAgua() {
+        return temperaturaAgua;
+    }
+
+    public void setTemperaturaAgua(double temperaturaAgua) {
+        this.temperaturaAgua = temperaturaAgua;
+    }
+
+    public double getpHAgua() {
+        return pHAgua;
+    }
+
+    public void setpHAgua(double pHAgua) {
+        this.pHAgua = pHAgua;
+    }
+
+    public double getOxigenacao() {
+        return oxigenacao;
+    }
+
+    public void setOxigenacao(double oxigenacao) {
+        this.oxigenacao = oxigenacao;
+    }
+
+    public double getPeixes() {
+        return peixes;
+    }
+
+    public void setPeixes(double peixes) {
         this.peixes = peixes;
     }
     
