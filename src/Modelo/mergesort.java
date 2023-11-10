3c7b35bd82abcaf65e66c9ea83d4a879e4b130eb
RodrigoS <2005rodrigosilva@gmail.com>
2023-11-10 19:57:38 -0300

    Criando Classe de Ordenação

--- src/Modelo/mergesort.java
+++ src/Modelo/mergesort.java
@@ -1,76 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package Modelo;
 
 import java.util.ArrayList;
+import java.util.List;
 
+/**
+ *
+ * @author dsm2
+ */
 public class mergesort {
 
-    public static void main(String[] args) {
-        ArrayList<Integer> v = new ArrayList<>();
-        v.add(14);
-        v.add(7);
-        v.add(8);
-        v.add(12);
-        v.add(9);
-        v.add(11);
-        v.add(6);
-        v.add(5);
-
-        mergeSort(v, 0, v.size() - 1);
-        imprimeVetor(v);
-    }
-
-    public static void mergeSort(int[] v, int esq, int dir) {
-        if (esq < dir) {
-            int meio = (esq + dir) / 2;
-            mergeSort(v, esq, meio);
-            mergeSort(v, meio + 1, dir);
-            merge(v, esq, meio, dir);
+    public static void mergeSort(List<Integer> lista, int esq, int dir) {
+        if (esq == dir) {
+            return;
         }
+        int meio = (esq + dir) / 2;
+        mergeSort(lista, esq, meio);
+        mergeSort(lista, meio + 1, dir);
+        merge(lista, esq, meio, dir);
     }
 
-    public static void merge(int[] v, int esq, int meio, int dir) {
-        int i, j, k;
+    public static void merge(List<Integer> lista, int esq, int meio, int dir) {
         int a_tam = meio - esq + 1;
         int b_tam = dir - meio;
 
-        int[] a = new int[a_tam];
-        int[] b = new int[b_tam];
+        List<Integer> a = new ArrayList<>(lista.subList(esq, esq + a_tam));
+        List<Integer> b = new ArrayList<>(lista.subList(meio + 1, meio + 1 + b_tam));
 
-        for (i = 0; i < a_tam; i++) {
-            a[i] = v[esq + i];
-        }
+        int i = 0, j = 0, k = esq;
 
-        for (j = 0; j < b_tam; j++) {
-            b[j] = v[meio + 1 + j];
-        }
-
-        i = 0;
-        j = 0;
-
-        for (k = esq; k <= dir; k++) {
-            if (i == a_tam) {
-                v[k] = b[j++];
-            } else if (j == b_tam || a[i] < b[j]) {
-                v[k] = a[i++];
+        for (; i < a_tam && j < b_tam; k++) {
+            if (a.get(i) < b.get(j)) {
+                lista.set(k, a.get(i++));
             } else {
-                v[k] = b[j++];
+                lista.set(k, b.get(j++));
             }
         }
-    }
 
-    public static void imprimeVetor(int[] v, int tam) {
-        System.out.println("\nOrdem correta: v = {");
-        for (int i = 0; i < tam; i++) {
-            System.out.printf("%d ", v[i]);
+        for (; i < a_tam; k++, i++) {
+            lista.set(k, a.get(i));
+        }
+
+        for (; j < b_tam; k++, j++) {
+            lista.set(k, b.get(j));
+        }
+    }
+/*
+    public static void imprimeLista(List<Integer> lista) {
+        System.out.println("\nOrdem correta: lista = {");
+        for (Integer elemento : lista) {
+            System.out.printf("%d ", elemento);
         }
         System.out.println("}");
     }
-
-    private static void mergeSort(ArrayList<Integer> v, int i, int i0) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    private static void imprimeVetor(ArrayList<Integer> v) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+*/
 }
